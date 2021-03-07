6e03dc45d0f0f03dab9d93a0a93e73afe8440668
Matheus Lenke <matheuslenke@gmail.com>
2021-03-07 16:04:56 -0300

    tentando husky com error

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>React avançado</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>NextJS</h1>
+    </div>
+  )
+}
+
+export default Home
